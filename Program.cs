6143551d9478ded6d01c6c3b6056268cf7373084
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-09-02 22:57:42 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone meuIphone = new Iphone("123456789", "iPhone 14", "ABCDEFGHIJKLMNOP", 128);
+meuIphone.Ligar();
+meuIphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Nokia meuNokia = new("9876543", "Nokia Tijolão", "ASFASDFWERQAS", 64);
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("Jogo da Cobrinha");
